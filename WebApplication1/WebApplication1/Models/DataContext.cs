9798de4ad754cc35e8f52b688320331f7e1bc879
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 08:49:39 -0400

    con MVC completo

---
+++ WebApplication1/WebApplication1/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebApplication1.Models.Student> Students { get; set; }
+    }
+
+
+}
---
+++ WebApplication1/WebApplication1/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        [Key]
+        public int studentID{ get; set; }
+        [Required]
+        [Display(Description ="Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Description ="First Name")]
+        public string FirstName { get; set; }
+    }
+}
